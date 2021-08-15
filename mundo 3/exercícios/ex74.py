6b1fba953ea6acadc06096448c9feec34f8250ae
BrUnknown <asafer.ferr@gmail.com>
2021-08-15 20:13:00 -0300

    add 74

--- mundo 3/exercícios/ex74.py
+++ mundo 3/exercícios/ex74.py
@@ -1,5 +1,6 @@
 import random
- tupl=(random.randint(-100,100), random.randint(-100,100), random.randint(-100,100), random.randint(-100,100), random.randint(-100,100))
+tupl = (random.randint(-100, 100), random.randint(-100, 100),
+        random.randint(-100, 100), random.randint(-100, 100), random.randint(-100, 100))
 
 
 print(f'os valores sorteados foram {tupl}')
