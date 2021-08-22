57d5e63098e2eaa49e6b55530375f8699287801e
BrUnknown <asafer.ferr@gmail.com>
2021-08-22 16:47:28 -0300

    atualização

--- lv3_Coleções_e_Funções/aulas/list_comprehension.py
+++ lv3_Coleções_e_Funções/aulas/list_comprehension.py
@@ -1,5 +1,11 @@
 
 
-l1 = [1, 2, 3, 4, 5, 6, 7]
+l1 = [1, 2, 3, 4, 5, 6, 7]  # define uma lista l1
+# executa um for para a cada valor lido da lista seja atribuido a var criando uma
 l2 = [var for var in l1]
-print(l2)
+# nova lista com essa variavel
+print(l2)  # print a reprodução de l1 se
+
+l3 = [va*2 for va in l1]  # le os valores e duplica na saida
+
+print(l3)
