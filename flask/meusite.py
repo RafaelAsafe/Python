5fba5e3afafcfdce243dc15e4fa5f3e76b820168
BrUnknown <asafer.ferr@gmail.com>
2021-08-22 15:02:45 -0300

    meusite

--- flask/meusite.py
+++ flask/meusite.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/inicial')
-def ola_mundo():
-    return render_template("index.html")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    Variavel = "Descubra os números"
+
+    if request.method == 'GET':
+        return render_template("index.html", Variavel=Variavel)
+    else:
+        Numero = [3.5, 4.5, 9.5, 3.5]
+        PalpiteA = float(request.form.get('name'))
+        PalpiteB = float(request.form.get('name1'))
+        PalpiteC = float(request.form.get('name2'))
+        PalpiteD = float(request.form.get('name3'))
+
+        if PalpiteA == Numero[0] and PalpiteB == Numero[1] and PalpiteC == Numero[2] and PalpiteD == Numero[3]:
+            return '<h1>Você Ganhou</h1>'
+        else:
+            return'<h1>Você Perdeu</h1>'
 
 
 @app.route('/sobre')
 def sobre():
-    return render_template("sobre.html")
+    return render_template('sobre.html')
 
 
 @app.route('/<string:nome>')
