302861e216bef3cacb6af5a1afc7964729376ef0
BrUnknown <asafer.ferr@gmail.com>
2022-04-28 13:25:04 -0300

    Interact OS

--- Interact_OS/Automation_example_3.py
+++ Interact_OS/Automation_example_3.py
@@ -5,14 +5,17 @@ import psutil
 def check_disk_usage(disk):
     du = shutil.disk_usage(disk)
     free = du.free / du.total *100
+    print(f'{free}/' )
     return free > 20
 
 def check_cpu_usage():
     usage = psutil.cpu_percent()
+    print(usage)
     return usage < 75
 
 if not check_cpu_usage() or check_disk_usage('/'):
     print("ERROR!")
+    
     
 else:
     print("Everthing is ok!")
